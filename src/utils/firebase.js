27460ab46eed7d06642fee5a27764e0f7445efbc
poyang1024 <poyang1024@gmail.com>
2025-10-22 01:17:20 +0800

    feat: update Firebase configuration for the new project setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIpR7w0X_lQBnqNzh4yGnIJLAURV2UY4E",
-  authDomain: "wanna-erp.firebaseapp.com",
-  projectId: "wanna-erp",
-  storageBucket: "wanna-erp.firebasestorage.app",
-  messagingSenderId: "51973078027",
-  appId: "1:51973078027:web:18f50196010c28d3dadb0e",
-  measurementId: "G-871ER9H187"
+  apiKey: "AIzaSyA1enj2ANF8HQ9tJPrk3KYbemFkI7Hh6ks",
+  authDomain: "wanna-erp-system.firebaseapp.com",
+  projectId: "wanna-erp-system",
+  storageBucket: "wanna-erp-system.firebasestorage.app",
+  messagingSenderId: "207021112139",
+  appId: "1:207021112139:web:4a255e1f18aacaf75b46ce"
 };
 
 firebase.initializeApp(firebaseConfig);
